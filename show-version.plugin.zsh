5dde74c70c068d5e8e40aecb638945bb9c58965c
Gustavo Tonietto <tonietto@gmail.com>
2017-03-07 15:02:19 -0300

    Add source-bash-commands function

--- show-version.plugin.zsh
+++ show-version.plugin.zsh
@@ -20,11 +20,18 @@ show-version() {
   fi
 }
 
+source-bash-commands() {
+  if [ -f bashcommands.sh ]; then
+    source bashcommands.sh
+  fi
+}
+
 chpwd () {
   local PKG
 
   PKG=$PWD/package.json
     if [ -f "$PKG" ] && [ "$NODENGINE_LAST_DIR" != "$PWD" ]; then
+      source-bash-commands
       show-version
       nodengine
       printf "\033[36m%s\033[0m \033[90m%s\033[0m\n" "node" "$(node --version)"
