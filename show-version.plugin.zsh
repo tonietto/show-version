465a5fba1523db0bea19681df723690262dd6101
Gustavo Tonietto <tonietto@gmail.com>
2017-03-22 17:43:02 -0300

    Fix minor error

--- show-version.plugin.zsh
+++ show-version.plugin.zsh
@@ -25,9 +25,9 @@ chpwd () {
 
   PKG=$PWD/package.json
     if [ -f "$PKG" ] && [ "$NODENGINE_LAST_DIR" != "$PWD" ]; then
+      show-version
       nodengine
       printf "\033[36m%s\033[0m \033[90m%s\033[0m\n" "node" "$(node --version)"
-      source-bash-commands
       NODENGINE_LAST_DIR=$PWD
     fi
 }
